051cdd60fd043bf5a696a7d55c6803fdae85ad48
Ignacio Fleming <ignacioflemings@gmail.com>
2024-04-13 19:50:00 -0300

    deleted comments

--- src/dao/repositories/payment.js
+++ src/dao/repositories/payment.js
@@ -1,12 +1,10 @@
 import Stripe from "stripe";
 import config from "../../config/config.js";
-// const Stripe = require("stripe")(config.stripe.secretApiKey);
 export default class PaymentService {
   constructor() {
     this.payment = Stripe(config.stripe.secretApiKey);
   }
   async createPaymentIntent(data) {
-    // console.log(ata);
     const paymentIntent = await this.payment.paymentIntents.create(data);
     return paymentIntent;
   }
