22ac62fe6a1c5926282f2dc3856434df840a8d33
Ignacio Fleming <ignacioflemings@gmail.com>
2024-04-14 20:10:00 -0300

    added get Tickets feature

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { passportCall } from "../middlewares/auth/passportCall.js";
 import { applyPolicy } from "../middlewares/policies/policies.js";
-import { getTicketsByPurchaser } from "../controllers/tickets.js";
+import { getAllTickets, getTicketsByPurchaser } from "../controllers/tickets.js";
 
 const router = Router();
 
-router.get("/", passportCall("jwt"), applyPolicy(["PUBLIC"]), getTicketsByPurchaser);
+router.get("/", passportCall("jwt"), applyPolicy(["ADMIN"]), getAllTickets);
+
+router.get("/:purchaser", passportCall("jwt"), applyPolicy(["PUBLIC", "ADMIN"]), getTicketsByPurchaser);
 
 export default router;
